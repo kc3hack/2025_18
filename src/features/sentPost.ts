f412efc2158b8d218322d038c25d4040c4799079
mafyyu <morita0126m@gmail.com>
2025-02-21 14:21:54 +0900

    引数の順番の変更

--- src/features/sentPost.ts
+++ src/features/sentPost.ts
@@ -15,6 +15,7 @@ export async function sentPost(
         .getPublicUrl(filePath);
 
         const imageUrl = urlData?.publicUrl;
+        console.log("imageurl",imageUrl)
 
         const {data,error} = await supabase
         .from("Post")
