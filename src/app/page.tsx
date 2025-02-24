3c4ff0cb0fbef9483c0ebd548b489d5526a16cde
tensho1026 <tensho1026@icloud.com>
2025-02-24 11:03:02 +0900

    color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
     savefunc();
   }, [user,isLoaded]);
   return (
-    <>
+    <div className="bg-white">
       <Header></Header>
       <ToggleSwitch></ToggleSwitch>
-    </>
+    </div>
   );
 }
